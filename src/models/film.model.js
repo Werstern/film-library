7c4d9b1c8d83d98987535bdd6dbb13ee63dd5f52
Ihor <wersternn@gmail.com>
2019-03-28 17:16:40 +0200

    Update film.model.js

--- src/models/film.model.js
+++ src/models/film.model.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-const server = 'ds111765.mlab.com:11765';
-const datebase = 'film-library';
-const user = 'theadvansedtestuser';
-const password = '8743b52063cd84097a65d1633f5c74f5';
+const config = require('../config.json');
 
-mongoose.connect(`mongodb://${user}:${password}@${server}/${datebase}`);
+mongoose.connect(`mongodb://${config.user}:${config.password}@${config.server}/${config.datebase}`);
 
 const FilmSchema = new mongoose.Schema({
   title: {
